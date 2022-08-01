0409cca8c30c5d7a89230c0d55f28ba5123ee18f
kosukeGun <ko.iwai09210921@icloud.com>
2022-08-01 18:19:27 +0900

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Memo;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        //メモ一覧を取得
+        $user = \Auth::user();
+        //ASC:昇順、DESC:降順
+        $memos = Memo::where("user_id",$user["id"])->where("status",1)->orderBy("updated_at","DESC")->get();
+        // dd($memos);
+        return view('home',compact("user","memos"));
+    }
+
+    public function create()
+    {
+        //ログインしているユーザ
+        $user = \Auth::user();
+        return view('create',compact("user"));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        // dd($data);//デバッグ関数
+        // POSTされたデータをDB（memosテーブル）に挿入
+        // MEMOモデルにDBへ保存する命令を出す
+
+        
+        //タグのIDが判明する
+        // タグIDをmemosテーブルに入れてあげる
+        $memo_id = Memo::insertGetId(['content' => $data['content'],'user_id' => $data['user_id'],'status' => 1]);
+        
+        // リダイレクト処理
+        return redirect()->route('home');
+    }
+}
