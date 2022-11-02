01661c910fc99adae8e62b7a9bba44e4ecd9afc6
kosukeGun <ko.iwai09210921@icloud.com>
2022-11-02 11:05:18 +0900

    【fix】画像のパスを変更2

--- resources/views/questionList.blade.php
+++ resources/views/questionList.blade.php
@@ -14,7 +14,7 @@
                     @foreach($memos_all as $memo)
                     <div class="col-sm-6 col-xs-12 col-md-4 col-lg-3">
                         <div class="card text-center m-4 ">
-                            <img width="100%" height="120px" src="{{ '/public/storage/' . $memo['image']}}">
+                            <img width="100%" height="120px" src="{{ '../storage/' . $memo['image']}}">
                             <div class="card-body">
                                 <h5 class="card-title">{{$memo["title_name"]}}</h5>
                                 <p class="card-text">{{$memo["user_name"]}}</p>
